39831680195fa2d6290ded352bd6ccb4dfba4472
nbhung58 <ngobaohung57@gmail.com>
2022-04-15 08:52:46 +0700

    last commit

--- library/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-SECRET_KEY = os.environ.get("KEY")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-#Nó không hiện thông báo khi làm việc với SQLALCHEMY
-SQLALCHEMY_TRACK_MODIFICATIONS = False
